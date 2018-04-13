cb9aa08077794df9a2aeab6cc4646f5a528987a6
Zhuanglijuan <zhuangljwork@163.com>
2018-04-13 15:57:30 +0800

    Pi-Estimation-Without-Rendering

---
+++ Pi-Estimation-Without-Rendering/src/Circle.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+/**
+ * Created by Zhuang on 2018/4/13.
+ */
+public class Circle {
+    private int x;
+    private int y;
+    private int r;
+
+    public Circle(int x, int y, int r) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public boolean contain(Point p) {
+        return Math.pow(p.getX() - x, 2) + Math.pow(p.getY() - y, 2) <= r * r;
+    }
+}
